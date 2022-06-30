53b781c55d6448d9603b2c5ca4218bb661276571
Suraj Tomar <58520961+tomarsuraj@users.noreply.github.com>
2022-06-30 20:33:25 +0530

    Editor Working

--- src/Components/SideCard.js
+++ src/Components/SideCard.js
@@ -1,3 +1,4 @@
+import { DeleteForever } from '@styled-icons/material';
 import React from 'react';
 
 const SideCard = ({ isActive, title = 'SIDE CARD', handleDelete, id }) => {
@@ -7,10 +8,16 @@ const SideCard = ({ isActive, title = 'SIDE CARD', handleDelete, id }) => {
         isActive ? 'activeNoteBookCard' : 'inActiveNoteBookCard'
       } d-flex justify-content-between`}
     >
-      <h5> {title} </h5>
-      <p className="text-danger" onClick={() => handleDelete(id)}>
-        Del
-      </p>
+      <h6> {title} </h6>
+
+      <DeleteForever
+        size={24}
+        onClick={(e) => {
+          e.stopPropagation();
+          handleDelete(id);
+        }}
+        className="text-danger"
+      />
     </div>
   );
 };
